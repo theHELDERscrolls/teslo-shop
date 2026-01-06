3f1314df6ce30e3bb7975f4efbdde4b31bf8068c
Helder Manuel Vieira Ruiz <manuhelderruiz@gmail.com>
2026-01-06 16:21:54 +0100

    tarjetas producto y fade in

---
+++ src/app/store-front/components/product-card/product-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'product-card',
+  imports: [RouterLink],
+  templateUrl: './product-card.html',
+})
+export class ProductCard {
+
+}
--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ProductCard } from "../../components/product-card/product-card";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './home-page.html',
 })
 export class HomePage {
