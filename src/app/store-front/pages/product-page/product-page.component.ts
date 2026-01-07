26d902c631f27f06ce527ceb5acd325f26bb03d5
Helder Manuel Vieira Ruiz <manuhelderruiz@gmail.com>
2026-01-07 13:19:15 +0100

    Carousel de imágenes

--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -2,10 +2,11 @@ import { ProductsService } from '@/products/services/products.service';
 import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
+import { ProductCarousel } from "@/products/components/product-carousel/product-carousel.component";
 
 @Component({
   selector: 'app-product-page',
-  imports: [],
+  imports: [ProductCarousel],
   templateUrl: './product-page.component.html',
 })
 export class ProductPage {
