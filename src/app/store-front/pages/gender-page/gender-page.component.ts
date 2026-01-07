f3580590ca345bfe350c6f5b71a14c0d3ce0ac5c
Helder Manuel Vieira Ruiz <manuhelderruiz@gmail.com>
2026-01-07 13:43:55 +0100

    página de productos por género

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { ProductsService } from '@/products/services/products.service';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductCard } from "@/store-front/components/product-card/product-card.component";
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.component.html',
 })
 export class GenderPage {
+  route = inject(ActivatedRoute);
 
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsService = inject(ProductsService);
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({ gender: params.gender });
+    },
+  });
 }
