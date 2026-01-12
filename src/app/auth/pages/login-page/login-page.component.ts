3cba3141ed6f75dc0314641f92ebaf2cf4c126cb
Helder Manuel Vieira Ruiz <manuhelderruiz@gmail.com>
2026-01-12 18:30:49 +0100

    formulario reactivo con autenticación

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink],
-  templateUrl: './login-page.component.html'
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './login-page.component.html',
 })
 export class LoginPage {
+  fb = inject(FormBuilder);
+  hasError = signal(false);
+  isPosting = signal(false);
 
+  // Nota: las validaciones del frontal han de ser lo más similares a las del back
+  // para evitar todas las interacciones innecesarias con el mismo.
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.minLength(6)]],
+  });
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.hasError.set(true);
+      setTimeout(() => {
+        this.hasError.set(false);
+      }, 2000);
+      return;
+    }
+
+    const { email = '', password = '' } = this.loginForm.value;
+
+    console.log(email, password);
+  }
 }
