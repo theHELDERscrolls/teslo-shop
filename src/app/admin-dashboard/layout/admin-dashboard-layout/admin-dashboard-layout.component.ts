cd97494f171443e5107a9dc84d64ac346222cca8
Helder Manuel Vieira Ruiz <manuhelderruiz@gmail.com>
2026-01-13 16:12:07 +0100

    diseño del panel admin

--- src/app/admin-dashboard/layout/admin-dashboard-layout/admin-dashboard-layout.component.ts
+++ src/app/admin-dashboard/layout/admin-dashboard-layout/admin-dashboard-layout.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@/auth/services/auth.service';
+import { Component, computed, inject } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard-layout',
-  imports: [RouterOutlet],
-  templateUrl: './admin-dashboard-layout.component.html'
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './admin-dashboard-layout.component.html',
 })
 export class AdminDashboardLayout {
+  authService = inject(AuthService);
 
+  user = computed(() => this.authService.user());
 }
