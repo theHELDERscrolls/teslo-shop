7d8db59069282d9624d544a8aa72a1112736df0c
Helder Manuel Vieira Ruiz <manuhelderruiz@gmail.com>
2026-01-13 11:32:02 +0100

    notAuthenticatedGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { notAuthenticatedGuard } from './auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes'),
-    // TODO: Guards
+    canMatch: [notAuthenticatedGuard],
   },
   {
     path: '',
---
+++ src/app/auth/guards/not-authenticated.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanMatchFn, Route, Router, UrlSegment } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { firstValueFrom } from 'rxjs';
+
+export const notAuthenticatedGuard: CanMatchFn = async (route: Route, segments: UrlSegment[]) => {
+  const authService = inject(AuthService);
+
+  const router = inject(Router);
+
+  const isAuthenticated = await firstValueFrom(authService.checkStatus());
+
+  if (isAuthenticated) {
+    router.navigateByUrl('/');
+    return false;
+  }
+
+  return true;
+};
